65da80429ec17814a03b7d4967cde80d5cb594eb
fanwang551 <96096907+fanwang551@users.noreply.github.com>
2022-09-02 20:43:39 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,133 +1,95 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// java review day1 9.1 write your code here
-     /*   System.out.println("HelloWorld");
-        System.out.println("书名\t作者\t价格\t销量\n小王子\t埃克苏佩\t20\t1000\t");
-        System.out.println("书名\t小王子\n作者\t埃克苏佩\n价格\t20\n销量\t1000");
-    */
-     //boolean数据类型
-      /*  boolean pass=true;
-        if(pass==true){
-            System.out.println("通过考试");}
-		   else{
-            System.out.println("未通过考试");}*/
+	// java review day2 9.2 目标：复习day1;完成程序控制结构
+        //  三元运算符  条件表达式？表达式1：表达式2；运算条件：条件表达式为true，运算后的结果是表达式1，否则为表达式2write your code here
+        /*表达式1和表达式2要为可以赋值给接收变量的类型；或者可以自动转换或强制转换
+        int a=1;
+        int b=2;
+        int c=a>b?(int)1.1:(int)2.2;
+        double d=a>b?a:b+3;//int→double
+        System.out.println(c);
+        System.out.println(d);*/
+
+        /*找最大值
+        int a=2;
+        int b=1;
+        int c=3;
+        int max=(a>b? a:b)>c? (a>b? a:b):c;
+        System.out.println(max);*/
 
 
-        //自动类型转换 五点
-        /*1.多种数据类型转换时，先转换陈精度高的再计算
-        int n1=10;
-        float d1=n1+1.1f;
-        double d2=n1+1.1;
-        System.out.println(d1);
-        System.out.println(d2);*/
-
-        /*2.不能将精度大的数据类型赋值给精度小的数据类型
-        int n=1.1;//错误
-        */
-
-        /*3.(byte  short)和char间不能相互转换
-        byte n=10;
-        int n1=10;
-        byte n2=n1;/错误
-        char c1=n;//错误
-        */
-          /*4.byte short char 间可以进行计算，计算时首先转换成int型
-          byte b1=1;
-          byte b2=2;
-          short s1=1;
-          int s2=b1+s1;
-          System.out.println(s2);
-          //byte b3=b1+B2;错误，首先转换成int*/
-
-        /*5.boolean不进行数据类型转换，表达式结果数据类型自动转换成操作数中最大的
-        */
+        /*java标识符命名规范
+        1.数字不可以开头
+        2.包名：多单词组合时所有字母小写
+        3.类名，接口名：多单词组合时，所有单词首字母大写 大驼峰
+        4.变量名，方法名：多单词组合时，第一个单词首字母小写，后面单词首字母大写，小驼峰
+        5。常量名：所用字母都大写，多单词用下划线连接
+         */
 
 
-      //强制类型转换
-        /*1.数据类大转小，char类型能保存int型常量值，但不能保存变量值
-        char c1=10;
-        int n=97;
-        char c2=(char)n;
-        System.out.println(c2);//10对应的字符，输出为a*/
-
-        /*基本数据类型转换成String
-        int n1=1;
-        float f1=1.1f;
-        double d1=1.2;
-        boolean b1=true;
-        String s1=n1+"";
-        String s2=f1+"";
-        String s3=d1+"";
-        String s4=b1+"";
-        System.out.println(s1+""+s2+""+s3+""+s4);*/
-
-        /*String转换成基本数据类型
-        String s5="20";
-        int n1=Integer.parseInt(s5);
-        double n2=Double.parseDouble(s5);
-        float n3=Float.parseFloat(s5);
-        boolean n4=Boolean.parseBoolean("true");
-        byte n5=Byte.parseByte(s5);//超过128 错误
-        System.out.println(n4);
-        //得到字符串某个位置的值
-        System.out.println(s5.charAt(0));*/
-
-        /*Homework
-        String BookName1="小王子";
-        String BookName2="红楼梦";
-        System.out.println("BookName"+BookName1+"\\"+BookName2);
-        String Name="王帆";
-        int Year=20;
-        int Grade=100;
-        String Sex="男";
-        String Hobby="Play basketball";
-        System.out.println("姓名"+"\t"+"年龄"+"\t"+"成绩"+"\t"+"性别"+"\t"+"爱好"
-                +"\n"+Name+"\t"+Year+"\t"+Grade+"\t"+Sex+"\t"+Hobby);*/
+        /*scanner的使用
+        Scanner sc=new Scanner(System.in);
+        System.out.println("input time");
+        String time=sc.next();
+        System.out.println("input month");
+        int  month=sc.nextInt();
+        System.out.println("当前时间是：");
+        System.out.println(time+month+"月");*/
+        /*移位运算
+        int a=1>>2;
+        int b=1<<2;
+        System.out.println(a+b);*/
 
 
-
-        //instanceof 检查是否是类对象；逻辑运算符最终结果为Boolean值
-
-
-        /*&&短路，如果第一个结果为false，后面条件不在判断；&逻辑与第一个条件为false，后面的条件仍会判断
-        int x=5;
-        int y=5;
-       // int x1=x++;
-       // int y1=++y;
-        if(x++==6&&++y==6){
-            x=11;
+        /*分支控制 if else
+        System.out.println("请输入年龄");
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>18){
+            System.out.println("已成年");
         }
-        System.out.println(y);//y=5,因为是短路与不会计算++y
-        if(x++==6&++y==6){
-            x=11;
+        else
+            System.out.println("未成年");
+        if(age>18){
+            System.out.println("已成年");
         }
-        System.out.println(y);//y=6,因为是逻辑与，会计算++y*/
+        else{
+            System.out.println("未成年");
+        }*/
 
 
-        /*一道经典例题
-        boolean x=true;
-        boolean y=false;
-        short s=46;
-        if((s++==46)&&(y=true)) {//s++已计算，s=47
-            s++;
+        /*practise
+        double x=12;
+        double y=18;
+       // method 1
+       if(x>10){
+            if(y<20){
+                System.out.println(x+y);
+            }
         }
-        if((x=false)||(++s==49)) {
-            s++;
+
+        if(x>10&&y<20){
+            System.out.println(x+y);
         }
-        System.out.println(s);s=50*/
+
+        if((x+y)%3==0&&(x+y)%5==0){
+            System.out.println("两数之和既能被三整除也能被五整除");
+        }
+
+        //判断闰年
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入年份");
+        int year=sc.nextInt();
+        if((year%4==0&&year%100!=0)||year%400==0){
+            System.out.println("是闰年");
+        }
+        else
+            System.out.println("不是闰年");*/
 
 
-        /*符合赋值运算符会进行类型转换
-        byte b=1;
-        b+=2;//b=(byte)(b+2)
-        b++;//b=(byte)(b+1)
-        System.out.println(b);*/
-
-
-
+        
     }
-
 }
